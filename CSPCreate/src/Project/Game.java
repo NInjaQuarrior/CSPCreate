15316cb02d780db1abd4820d988f6e206a20822f
Conner McKevitt <cmmckevitt@gmail.com>
2018-03-18 15:34:29 -0400

    Created main and runGame

--- CSPCreate/src/Project/Game.java
+++ CSPCreate/src/Project/Game.java
@@ -1,5 +1,22 @@
 package Project;
+import java.awt.*;
 
+import javax.swing.JFrame;
 public class Game {
+	boolean isRunningGame = true;
+	boolean firstRun = true;
 
+	
+	public void runGame() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Game game = new Game();
+					
+			game.runGame();
+	
+	}
+	
 }
